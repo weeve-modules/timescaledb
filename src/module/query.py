6e41cf599e6e19957475b4dfc24c9eb48daa69a1
kubagrzelak <jakubgrzelak1@gmail.com>
2023-01-15 21:51:58 +0000

    Correct way of declaring SQL queries

--- src/module/query.py
+++ src/module/query.py
@@ -1,9 +1,9 @@
-import psycopg2
 from psycopg2 import sql
 from .params import PARAMS
 
 # build SQL query
-QUERY = sql.SQL(f"INSERT INTO {PARAMS['TABLE_NAME']}" + " ({columns}) VALUES ({values});").format(
+QUERY = sql.SQL("INSERT INTO {table} ({columns}) VALUES ({values});").format(
+    table=sql.Identifier(PARAMS['TABLE_NAME']),
     columns=sql.SQL(', ').join(map(sql.Identifier, PARAMS["COLUMNS"])),
     values=sql.SQL(', ').join(sql.Placeholder() * len(PARAMS["LABELS"]))
 )
